2f8c5dafa089e34a0bf60af833bfca29ddbb6fe5
mhmmedinan <minan.65@hotmail.com>
2025-04-26 12:16:49 +0300

    Business ve WebAPI katmanında Service ve controller yapıları oluşturuldu. Request-Response Pattnert uygulandı

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,8 @@
+using Business.Abstracts;
+using Business.Concretes;
 using Microsoft.EntityFrameworkCore;
+using Repositories.Abstracts;
+using Repositories.Concretes;
 using Repositories.Concretes.EntityFramework.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +11,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<BaseDbContext>(op=>op.UseSqlServer(builder.Configuration.GetConnectionString("BaseDb")));
 
+builder.Services.AddScoped<IBrandService, BrandManager>();      //Her Http request için bir kez oluþturulur
+builder.Services.AddScoped<IBrandRepository, BrandRepository>();
+
+//AddSingleton   => Uygulama baþladýðýnda bir kez oluþturulur. Cache iþlemleri Config ayarlarýný yöneten servisler
+//AddTransiet => Her kullanýmda yeni bir nesne oluþturur.EmailSenderService
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Add services to the container.
+
+
+// Uygulamayý yapýlandýrýr
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 
