ed5acd5eec8b5d043d341f383ba253e9bfeba8f6
mhmmedinan <minan.65@hotmail.com>
2025-05-03 18:44:10 +0300

    AutoMapper implementasyonu

---
+++ Business/Abstracts/IModelService.cs
@@ -0,0 +1,11 @@
+﻿using Business.Dtos.Requests.Model;
+using Business.Dtos.Responses.Model;
+
+namespace Business.Abstracts;
+
+public interface IModelService
+{
+
+    CreatedModelResponse Add(CreateModelRequest request);
+    List<GetListModelResponse> GetList();
+}
--- Business/Concretes/BrandManager.cs
+++ Business/Concretes/BrandManager.cs
@@ -1,4 +1,5 @@
-﻿using Business.Abstracts;
+﻿using AutoMapper;
+using Business.Abstracts;
 using Business.Dtos.Requests.Brand;
 using Business.Dtos.Responses.Brand;
 using Entities;
@@ -9,46 +10,64 @@ namespace Business.Concretes;
 public class BrandManager : IBrandService
 {
     private readonly IBrandRepository _brandRepository;
+    private readonly IMapper _mapper;
 
-    public BrandManager(IBrandRepository brandRepository)
+    public BrandManager(IBrandRepository brandRepository, IMapper mapper)
     {
         _brandRepository = brandRepository;
+        _mapper = mapper;
     }
 
     public CreatedBrandResponse Add(CreateBrandRequest request)
     {
-        //Manual Mapping
-        Brand brand = new Brand
-        {
-            Name = request.Name
-        };
-        Brand addedBrand = _brandRepository.Add(brand);
+        ////Manual Mapping
+        //Brand brand = new Brand
+        //{
+        //    Name = request.Name
+        //};
+        //Brand addedBrand = _brandRepository.Add(brand);
 
-        return new CreatedBrandResponse
-        {
-            Id = addedBrand.Id,
-            Name = addedBrand.Name,
-            CreatedDate = addedBrand.CreatedDate
-        };
+        //return new CreatedBrandResponse
+        //{
+        //    Id = addedBrand.Id,
+        //    Name = addedBrand.Name,
+        //    CreatedDate = addedBrand.CreatedDate
+        //};
+
+
+        //AutoMapper
+
+        Brand brand = _mapper.Map<Brand>(request);
+        Brand createdBrand = _brandRepository.Add(brand);
+        CreatedBrandResponse response = _mapper.Map<CreatedBrandResponse>(createdBrand);
+        return response;
+
     }
 
     public List<GetListBrandResponse> GetList()
     {
         //Manual Mapping
-        List<Brand> brands = _brandRepository.GetAll();
-        List<GetListBrandResponse> responses = new List<GetListBrandResponse>();
+        //List<Brand> brands = _brandRepository.GetAll();
+        //List<GetListBrandResponse> responses = new List<GetListBrandResponse>();
 
-        foreach(var brand in brands)
-        {
-            responses.Add(new GetListBrandResponse
-            {
-                Id = brand.Id,
-                Name = brand.Name,
-                CreatedDate = brand.CreatedDate,
-                UpdatedDate = brand.UpdatedDate
-            });
-        }
+        //foreach(var brand in brands)
+        //{
+        //    responses.Add(new GetListBrandResponse
+        //    {
+        //        Id = brand.Id,
+        //        Name = brand.Name,
+        //        CreatedDate = brand.CreatedDate,
+        //        UpdatedDate = brand.UpdatedDate
+        //    });
+        //}
+        //return responses;
+
+
+        //AutoMapper
+        List<Brand> brands = _brandRepository.GetAll();
+        List<GetListBrandResponse> responses = _mapper.Map<List<GetListBrandResponse>>(brands);
         return responses;
+
     }
 
     //public void Add(Brand brand)
